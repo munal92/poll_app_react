eb4d95911e0d41c1f8aab33dd742a1fb0447b4fc
unknown <m_fattih@hotmail.com>
2020-06-24 21:29:44 -0700

    Analytics added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./components/Home";
 import Navigbar from "./components/NavigBar";
 import Footer from "./components/Footer";
 import { Switch, Route } from "react-router-dom";
 import PollResult from "./components/PollResult";
 import { ToastContainer, toast } from "react-toastify";
+import ReactGa from "react-ga";
 function App() {
+  useEffect(() => {
+    ReactGa.initialize("UA-170271378-2");
+
+    ReactGa.pageview(window.location.pathname);
+    // + window.location.search
+  }, []);
   return (
     <>
       <Navigbar />
